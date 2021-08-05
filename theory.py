ec50babf13f88bbff71b6e2d019d66d7fd531f27
AndrewAMur <87572899+AndrewMuratov@users.noreply.github.com>
2021-08-05 13:31:59 +0000

    Added Gitignore file, Proof.py, and updated Theory.py

--- theory.py
+++ theory.py
@@ -6,18 +6,17 @@ threeXone.append(userInput)
 print(str(userInput))
 
 while userInput != 1:
-  if userInput % 2 == 1:
-    userInput *= 3
-    userInput += 1
-    print(userInput)
-    threeXone.append(userInput)
-  else:
-    userInput /= 2
-    userInput = int(userInput)
-    print(str(userInput))
-    threeXone.append(userInput)
-  i += 1
+    if userInput % 2 == 1:
+        userInput *= 3
+        userInput += 1
+        print(userInput)
+        threeXone.append(userInput)
+    else:
+        userInput /= 2
+        userInput = int(userInput)
+        print(str(userInput))
+        threeXone.append(userInput)
+    i += 1
 
 i = str(i)
 print(f"\nThe number of operations is: {i}")
-print(f"\n{threeXone}")
